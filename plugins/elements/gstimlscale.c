24b5d226ddee8fc1d763c517bf966e2bb8fc0668
imlgit-debug <57432772+imlgit-debug@users.noreply.github.com>
2021-04-20 10:32:26 +0900

    Update gstimlscale.c

--- plugins/elements/gstimlscale.c
+++ plugins/elements/gstimlscale.c
@@ -35,8 +35,6 @@
 #endif
 
 #include <gst/gst.h>
-#include <gst/video/video.h>
-#include <gst/video/gstvideofilter.h>
 #include "gstimlscale.h"
 
 GST_DEBUG_CATEGORY_STATIC (gst_imlscale_debug_category);
